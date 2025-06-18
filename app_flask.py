d5b0b8d3036d1c3079b8d6d819fa4548b8499793
Luis Amézquita <luisamezquitaap@gmail.com>
2025-06-18 17:44:24 -0400

    v1 - Versión Flask estable

--- app_flask.py
+++ app_flask.py
@@ -1,34 +1,34 @@
-from quart import Quart, render_template, request
-#Creating a quart instancy named app
-app = Quart (__name__)
+from flask import Flask, request, render_template
+# Creating a flask instancy named app
+app = Flask(__name__)
 
 # Main app route
 @app.get('/')
-async def home():
-    return await render_template('home.html')
-
+def home():
+    return render_template('home.html')
+    
 # About us
 @app.get('/about')   
-async def about():
-    return await render_template('about.html')
+def about():
+    return render_template('about.html')
 
 # Contact
 @app.get('/contact')
-async def contact():
-    return await render_template('contact.html')
+def contact():
+    return render_template('contact.html')
 
 # Greetings with name using query string
 @app.get('/hello')
-async def greetings():
+def greetings():
     name = request.args.get('name')  # Get name parameter from the URL
-    return await render_template('hello.html', name=name)
+    return render_template('hello.html', name=name)
 
 # Product by ID using rute parameters
 @app.get('/products/<int:product_id>')
-async def products(product_id):
+def products(product_id): # Get id parameter from the URL
     return f"El id del producto que buscas es: {product_id}"
-
 
 # To run the server
 if __name__ == '__main__':
     app.run(debug=True)
+    
